6ea52c68c34bd355f129462f72dd1eaa78d803d4
roydrori <roydrori75@gmail.com>
2023-07-09 12:46:40 +0300

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { AuthContextProvider } from './auth/authContext';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.defaults.baseURL = 'https://netflix-backend-qdjy.onrender.com';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthContextProvider>
